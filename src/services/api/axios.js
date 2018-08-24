c95753c1c9c87cc04b04bb7cb5dfb06d1a69f19d
Hadi Hidayat Hammurabi <didikipb007@gmail.com>
2018-08-24 22:42:56 +0700

    Change API address

--- src/services/api/axios.js
+++ src/services/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const api = axios.create({
   //baseURL: 'http://localhost:3000/',
   //baseURL: 'http://192.168.100.28:3000/',
-  baseURL: 'http://35.231.229.161:3000/',
+  baseURL: 'http://192.3.130.45:3333/',
   timeout: 3000
 })
 
